44cbd408ea68c63ab84e182b2beb1b0bf2a71a62
ilyuzaaaaa <61176809+ilyuzaaaaa@users.noreply.github.com>
2020-04-06 23:56:25 +0500

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-  std::string name;
-  std::cin>>name; //insert name
+  std::string name;//переменная для имени
+  std::cin>>name; //введите имя
   std::cout << "Hello World from" <<name<<std::endl;
   return 0;
 }
